640f2d7394266bc7f1e27c13fba624111e104480
Manu J <manu.1982@gmail.com>
2011-11-22 17:01:11 +0530

    raw urls have changed

--- setup_env.sh
+++ setup_env.sh
@@ -5,7 +5,7 @@
 # bash < <( curl  https://github.com/boundlessdreamz/dotfiles/raw/master/setup_env.sh )
 
 cd $HOME
-wget --no-check-certificate https://github.com/boundlessdreamz/dotfiles/raw/master/vim_minimal
-wget --no-check-certificate  https://github.com/boundlessdreamz/dotfiles/raw/master/screenrc
+wget --no-check-certificate https://raw.github.com/j-manu/dotfiles/master/vim_minimal
+wget --no-check-certificate  https://raw.github.com/j-manu/dotfiles/master/screenrc
 mv vim_minimal .vimrc
 mv screenrc .screenrc
